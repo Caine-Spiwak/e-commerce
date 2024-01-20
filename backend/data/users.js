a054fae869d47852b5ef38a6cf29c1050357caf5
Caine <143353083+Caine-Spiwak@users.noreply.github.com>
2024-01-20 13:45:02 -0800

    Validate User email and password - auth route

--- backend/data/users.js
+++ backend/data/users.js
@@ -4,19 +4,19 @@ const users = [
   {
     name: 'Admin User',
     email: 'admin@email.com',
-    password: bcrypt.hashSync('1234546', 10),
+    password: bcrypt.hashSync('123456', 10),
     isAdmin: true,
   },
   {
     name: 'John Doe',
     email: 'john@email.com',
-    password: bcrypt.hashSync('1234546', 10),
+    password: bcrypt.hashSync('123456', 10),
     isAdmin: false,
   },
   {
     name: 'Jane Doe',
     email: 'Jane@email.com',
-    password: bcrypt.hashSync('1234546', 10),
+    password: bcrypt.hashSync('123456', 10),
     isAdmin: false,
   },
 ]
