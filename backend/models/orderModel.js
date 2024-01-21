760dd871423f2800df25a40ab7fd5cd22c4140a2
Caine <143353083+Caine-Spiwak@users.noreply.github.com>
2024-01-21 14:07:10 -0800

    Checkout Process

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -22,7 +22,7 @@ const orderSchema = mongoose.Schema({
   shippingAddress: {
     address: { type: String, required: true },
     city: { type: String, required: true },
-    PostalCode: { type: String, required: true },
+    postalCode: { type: String, required: true },
     country: { type: String, required: true },
   },
   paymentMethod: {
